5119dce7d00cd1a6e283c146bf2632674a55a39e
Eucalyptus5 <victorfuan197@gmail.com>
2024-04-17 17:09:34 -0700

    first

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,12 @@
-use std::io::{self};
-use std::iter::Peekable;
-use std::str::Chars;
+mod tokenizer;
+use tokenizer::{Tokenizer, Token};
 
 fn main() {
-    let mut input = String::new();
-    println!("Enter Expression: ");
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    let mut input = input.trim().chars().peekable();
-    let result = parse_computation(&mut input);
-    println!("Result: {}", result);
-}
-
-fn parse_computation(input: &mut Peekable<Chars>) -> i32 {
-    let result = parse_expression(input);
-    match_char(input, '.');
-    result
-}
-
-fn parse_expression(input: &mut Peekable<Chars>) -> i32 {
-    let mut result = parse_term(input);
+    let mut tokenizer = Tokenizer::new("var x = 100 + 20 - 2;");
     loop {
-        let next_char = input.peek();
-        match next_char {
-            Some('+') => {
-                input.next();
-                result += parse_term(input);
-            }
-            Some('-') => {
-                input.next();
-                result -= parse_term(input);
-            }
-            Some(' ') => {
-                skip_whitespace(input);
-            }
-            _ => break,
-        }
-    }
-
-    result
-}
-
-fn parse_term(input: &mut Peekable<Chars>) -> i32 {
-    let mut result = parse_factor(input);
-    loop {
-        let next_char = input.peek();
-        match next_char {
-            Some('*') => {
-                input.next();
-                result *= parse_factor(input);
-            }
-            Some('/') => {
-                input.next();
-                let divisor = parse_factor(input);
-                if divisor == 0 {
-                    panic!("Divide by zero error");
-                }
-                result /= divisor;
-            }
-            Some(' ') => {
-                skip_whitespace(input);
-            }
-            _ => break,
-        }
-    }
-
-    result
-}
-
-fn parse_factor(input: &mut Peekable<Chars>) -> i32 {
-    skip_whitespace(input);
-    if let Some(&next_char) = input.peek() {
-        match next_char {
-            '(' => {
-                input.next();
-                let result = parse_expression(input);
-                skip_whitespace(input);
-                match_char(input, ')');
-                result
-            }
-            _ => parse_number(input),
-        }
-    } else {
-        panic!("Unexpected end");
-    }
-}
-
-fn parse_number(input: &mut Peekable<Chars>) -> i32 {
-    let mut number = 0;
-    // Loop while the next character is a digit, consuming only digits.
-    while let Some('0'..='9') = input.peek() {
-        // Safely unwrap the digit now, knowing it's there and is a digit.
-        if let Some(digit_char) = input.next() {
-            // Convert the character to a digit and update the number.
-            let digit = digit_char.to_digit(10).unwrap() as i32;
-            number = number * 10 + digit;
-        }
-    }
-    number
-}
-
-fn match_char(input: &mut Peekable<Chars>, expected: char) {
-    match input.next() {
-        Some(c) if c == expected => (),
-        _ => panic!("Expected '{}' but found something else.", expected),
-    }
-}
-
-fn skip_whitespace(input: &mut Peekable<Chars>) {
-    while let Some(next_char) = input.peek() {
-        if next_char.is_whitespace() {
-            input.next();
-        } else {
+        let token = tokenizer.next_token();
+        println!("{:?}", token);
+        if token == Token::EOF {
             break;
         }
     }
---
+++ src/tokenizer.rs
@@ -0,0 +1,91 @@
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Number(i64),
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Identifier(String),
+    Assignment,
+    Semicolon,
+    Var,
+    OpenParen,
+    CloseParen,
+    EOF,
+}
+
+pub struct Tokenizer<'a> {
+    input: &'a str,
+    pos: usize,
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Tokenizer { input, pos: 0 }
+    }
+
+    pub fn peek(&self) -> char {
+        self.input.chars().nth(self.pos).unwrap_or('\0')
+    }
+
+    fn next_char(&mut self) -> char {
+        let c = self.input.chars().nth(self.pos).unwrap_or('\0');
+        self.pos += 1;
+        c
+    }
+
+    fn consume_while<F>(&mut self, test: F) -> String
+    where
+        F: Fn(char) -> bool,
+    {
+        let mut result = String::new();
+        while test(self.peek()) && self.peek() != '\0' {
+            result.push(self.next_char());
+        }
+        result
+    }
+
+    fn consume_whitespace(&mut self) {
+        self.consume_while(|c| c.is_whitespace());
+    }
+
+    fn tokenize_number(&mut self) -> Token {
+        let number_str = self.consume_while(|c| c.is_digit(10));
+        Token::Number(number_str.parse::<i64>().unwrap())
+    }
+
+    fn tokenize_identifier_or_keyword(&mut self) -> Token {
+        let identifier = self.consume_while(|c| c.is_alphanumeric() || c == '_');
+        match identifier.as_str() {
+            "var" => Token::Var,
+            _ => Token::Identifier(identifier),
+        }
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        self.consume_whitespace();
+        let c = self.peek();
+        match c {
+            '0'..='9' => self.tokenize_number(),
+            '+' => { self.next_char(); Token::Plus },
+            '-' => { self.next_char(); Token::Minus },
+            '*' => { self.next_char(); Token::Star },
+            '/' => { self.next_char(); Token::Slash },
+            ';' => { self.next_char(); Token::Semicolon },
+            '(' => { self.next_char(); Token::OpenParen },
+            ')' => { self.next_char(); Token::CloseParen },
+            '<' => {
+                self.next_char();
+                if self.peek() == '-' {
+                    self.next_char();
+                    Token::Assignment
+                } else {
+                    panic!("Unexpected character after '<': {}", self.peek());
+                }
+            },
+            _ if c.is_alphabetic() => self.tokenize_identifier_or_keyword(),
+            '\0' => Token::EOF,
+            _ => panic!("Unexpected character: {}", c),
+        }
+    }
+}
